cc4cfc2442d37eaaa6bd122edd895c25b1ea28e4
Md. Syed Afridi <83403859+SyedAfridi007@users.noreply.github.com>
2022-03-04 21:58:41 +0600

    Delete Popup Modal directory

--- Popup Modal/Popup Center Zoom/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector("#open-popup").addEventListener("click",function(){
-    document.querySelector(".popup").classList.add("active");
-  });
-  document.querySelector(".popup .close-btn").addEventListener("click",function(){
-    document.querySelector(".popup").classList.remove("active");
-  });
